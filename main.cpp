7432834b7b100efff2063c1255cdfbae434932cc
Timothy Vu <timothy.vu@sjsu.edu>
2021-04-24 11:33:52 -0700

    pushing minor changes

--- main.cpp
+++ main.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
-#include "card.h"
 #include "Blockchain.h"
 #include "game.h"
 using namespace std;
 
 int main(){
-    // Card game;
-    // game.run();
+
     game play;
     play.run1();
 
