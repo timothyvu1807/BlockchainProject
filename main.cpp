930bcc6ec0b3bdb53199f605d84853278beeeec9
Andy Ngo <Andyngo28@gmail.com>
2021-05-01 11:43:18 -0700

    Commit

--- main.cpp
+++ main.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
-#include "card.h"
 #include "Blockchain.h"
 #include "game.h"
 using namespace std;
 
-int main(){
-    // Card game;
-    // game.run();
-    game play;
-    play.run1();
-
+int main()
+{
+  // Card game;
+  // game.run();
+  game play;
+  play.run1();
 }
